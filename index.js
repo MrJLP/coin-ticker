f7fad0034b15800f463a14e8f31a5d5641d21d8a
Jason Phillips <jason_phillips@unseen.is>
2017-04-05 05:17:11 +0000

    removed unnecessary exchanges

--- index.js
+++ index.js
@@ -1,13 +1,6 @@
 const coinbase = require('./exchanges/coinbase');
 const bitstamp = require('./exchanges/bitstamp');
 
-const bitfinex = require('./exchanges/bitfinex');
-const btce = require('./exchanges/btce');
-const kraken = require('./exchanges/kraken');
-const okcoin = require('./exchanges/okcoin');
-const exmo = require('./exchanges/exmo');
-const poloniex = require('./exchanges/poloniex');
-
 function coinTicker(exchange, pair) {
   switch (exchange) {
     case 'coinbase':
@@ -17,24 +10,6 @@ function coinTicker(exchange, pair) {
       return bitstamp(pair);
       break;
 
-    case 'bitfinex':
-      return bitfinex(pair);
-      break;
-    case 'btce':
-      return btce(pair);
-      break;
-    case 'kraken':
-      return kraken(pair);
-      break;
-    case 'poloniex':
-      return poloniex(pair);
-      break;
-    case 'okcoin':
-      return okcoin(pair);
-      break;
-    case 'exmo':
-      return exmo(pair);
-      break;
     default:
       console.error(`Unrecognized exchange: "${exchange}"`);
       return 'Unrecognized exchange';
